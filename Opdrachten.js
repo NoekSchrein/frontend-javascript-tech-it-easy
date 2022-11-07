ec19ae1c0de927f6be0e9631fd02585a767851a2
NoekSchrein <noek.schrein@gmail.com>
2022-11-07 19:41:32 +0100

    opdracht 4

--- Opdrachten.js
+++ Opdrachten.js
@@ -84,8 +84,8 @@ toBeSoldToRed.textContent = tvsToBeSold;
 
 function getBrandsList(array) {
 
-    const arrayTvBrands = inventory.map((inventoryitem) => {
-        return inventoryitem.brand;
+    const arrayTvBrands = inventory.map((inventoryItem) => {
+        return inventoryItem.brand;
     })
 
     console.log(arrayTvBrands);
@@ -105,12 +105,33 @@ getBrandsList(inventory);
 
 // Opdracht 4a: Maak een herbruikbare functie die een string genereert voor de naam van één tv en deze teruggeeft in het format [merk] [type] - [naam] zoals Philips 43PUS6504/12 - 4K TV of NIKKEI NH3216SMART - HD smart TV.
 
-
+function tvToString(tv) {
+    return `${tv.brand} ${tv.type} - ${tv.name}`
+}
 
 // Opdracht 4b: Maak een herbruikbare functie die de prijs van één tv als parameter verwacht (zoals 379 of 159) teruggeeft in het format €379,- of €159,-.
 
+function priceNotation(price) {
+    return `€${price},-`
+}
 
 // Opdracht 4c: Maak een herbruikbare functie die een string genereert voor alle beschikbare schermgroottes van één tv. De functie geeft dit terug in het format [schermgrootte] inches ([schermgrootte omgerekend]cm) | [schermgrootte] inches ([schermgrootte omgerekend]cm) etc. Als een tv maar één schermgrootte heeft ([32]) wordt de output 32 inch (81 cm). Wanneer een tv vier schermgroottes heeft ([43, 50, 55, 58]) wordt de output 43 inch (109 cm) | 50 inch (127 cm) | 58 inch (147 cm). Let op: om één string te genereren uit een array van schermgroottes zul je een for-loop voor moeten gebruiken.
+
+function screenSizesToString (screenSizes) {
+    let sizeString = " ";
+
+    for (let i = 0; i < screenSizes.length; i++) {
+        const screenSizesInches = screenSizes[i];
+        const screenSizesCm = screenSizes[i] * 2.54;
+
+        sizeString = `${screenSizesInches} inches (${screenSizesCm} cm)`;
+
+        if (i < screenSizes.length -1) {
+            sizeString = `${sizeString} | `;
+        }
+    }
+    return sizeString;
+}
 
 // Opdracht 4d: Schrijf een script die de informatie van de Philips 43PUS6504/12 tv weergeeft op de pagina zoals onderstaand voorbeeld. Gebruik de functies die je hebt gemaakt in opdracht 4a, 4b en 4c.
 
@@ -118,4 +139,28 @@ getBrandsList(inventory);
 // €379,-
 // 43 inch (109 cm) | 50 inch (127 cm) | 58 inch (147 cm)
 
+const tvInformationOnPage = document.getElementById("tv-formation");
+tvInformationOnPage.textContent = tvToString({type: '43PUS6504/12', name: '4K TV', brand: 'Philips'});
+
+const tvPriceOnPage = document.getElementById("price");
+tvPriceOnPage.textContent = priceNotation(379)
+
+const tvSizesOnPage = document.getElementById("screen-sizes");
+tvSizesOnPage.textContent = screenSizesToString([43, 50, 58]);
+
+
 // Opdracht 4e: Maak een herbruikbare functie die de informatie van alle tv's weergeeft op de pagina. Gebruik hiervoor de map-methode in combinatie met de functies die je hebt gemaakt in opdracht 4a, 4b en 4c.
+
+
+function getTvsOnPage (arrayOfTvs) {
+    const layoutTvs = arrayOfTvs.map((tv) => {
+        return `${tv.brand}
+    ${tv.type}
+    ${tv.price}
+    `})
+
+    const tvListOnPage = document.getElementById("list-of-tvs");
+    tvListOnPage.textContent =
+}
+
+
